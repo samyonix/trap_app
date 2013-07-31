a921f414040d9558733cb36f9400ee67ec528ca6
samy <samyonix@gmail.com>
2013-07-31 11:11:54 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrapApp::Application.config.secret_token = '7470dbf4ea5674507bb38d58b27b019c7ef235108bf8a1c2f9e46af96bbe67e3f5cf2d696439c0578f5066930b38da9fa92dfb02e09b4e328e564da8a6e8d305'
